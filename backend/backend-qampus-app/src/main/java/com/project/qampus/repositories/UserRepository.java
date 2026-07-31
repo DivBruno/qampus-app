cce22662f63084089199953e5d3e4decd020fb85
oalencar <oalencardev@gmail.com>
2026-07-31 16:21:20 -0300

    git commit -m "feat(acess): configuração do spring security"

--- backend/backend-qampus-app/src/main/java/com/project/qampus/repositories/UserRepository.java
+++ backend/backend-qampus-app/src/main/java/com/project/qampus/repositories/UserRepository.java
@@ -4,7 +4,11 @@ import com.project.qampus.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.lang.ScopedValue;
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User,String> {
+    Optional<User> findByEmail(String email);
 }
