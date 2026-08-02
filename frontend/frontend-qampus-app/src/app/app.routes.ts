aed6eaf612d3ba5f208d0a41c3e3d1228ee84f0a
GabrieSVasc <vascgabrie@gmail.com>
2026-08-02 20:41:05 -0300

    feat(auth): implementa a tela unauthorized

--- frontend/frontend-qampus-app/src/app/app.routes.ts
+++ frontend/frontend-qampus-app/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Register } from './auth/register/register';
+import { Unauthorized } from './auth/unauthorized/unauthorized';
 
 export const routes: Routes = [
     {
         path: 'registrar',
         component: Register
+    },
+    {
+        path: 'unauthorized',
+        component: Unauthorized
     }
 ];
