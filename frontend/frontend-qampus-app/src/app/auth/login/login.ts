2c8bee13b5c7e90e05fd9b7394e644c33d8dc4f7
Pedro-Henrique-Da-Silva-Farias <pedrohsfarias27@gmail.com>
2026-08-03 18:07:13 -0300

    Criando o login frontend

---
+++ frontend/frontend-qampus-app/src/app/auth/login/login.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+  email = '';
+  senha = '';
+
+  onSubmit() {
+    console.log('Email:', this.email);
+    console.log('Senha:', this.senha);
+  }
+}
