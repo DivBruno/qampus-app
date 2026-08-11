3334b3c70dcae66a6c5c1bad121e88a9dba0e17b
Divancy Bruno <bruno-091@hotmail.com>
2026-08-11 16:42:28 -0300

    Feat(find post) - implementado a funcionalidade de visualizar posts, individualmente ou todos

--- backend/backend-qampus-app/src/main/java/com/project/qampus/controllers/PostController.java
+++ backend/backend-qampus-app/src/main/java/com/project/qampus/controllers/PostController.java
@@ -3,26 +3,34 @@ package com.project.qampus.controllers;
 import com.project.qampus.dto.PostDTO;
 import com.project.qampus.dto.PostResponseDTO;
 import com.project.qampus.model.Post;
-import com.project.qampus.repositories.QuestionRepository;
+import com.project.qampus.repositories.PostRepository;
 import com.project.qampus.service.PostService;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import jakarta.validation.Valid;
+
+import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
-@RequestMapping("/question")
+@RequestMapping("/post")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:8080")
 public class PostController {
 
-    private final QuestionRepository repository;
+    private final PostRepository repository;
     private final PostService postService;
 
     @PostMapping("/create")
@@ -30,4 +38,19 @@ public class PostController {
         Post saved = postService.create(body);
         return ResponseEntity.status(HttpStatus.CREATED).body(PostResponseDTO.from(saved));
     }
+
+    @GetMapping
+    public ResponseEntity<List<PostResponseDTO>> getPosts() {
+        List<PostResponseDTO> posts = postService.findAll().stream().map(PostResponseDTO::from).toList();
+
+        return ResponseEntity.ok(posts);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponseDTO> getPosts(@PathVariable String id){
+        Post post = postService.findById(id);
+        
+        return ResponseEntity.ok(PostResponseDTO.from(post));
+    }
+
 }
