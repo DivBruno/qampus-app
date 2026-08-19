18d99e2b8fa4f6cfdc15d22aa9fa4f021d588185
GabrieSVasc <vascgabrie@gmail.com>
2026-08-19 20:41:21 -0300

    fix: ajustando arquivos

--- backend/backend-qampus-app/src/main/java/com/project/qampus/controllers/AuthController.java
+++ backend/backend-qampus-app/src/main/java/com/project/qampus/controllers/AuthController.java
@@ -18,7 +18,6 @@ import com.project.qampus.dto.RegisterRequestDTO;
 import com.project.qampus.dto.ResponseDTO;
 import com.project.qampus.model.BlacklistedToken;
 import com.project.qampus.model.User;
-//import com.project.qampus.model.enums.Role;
 
 import jakarta.servlet.http.HttpServletRequest;
 
--- backend/backend-qampus-app/src/main/java/com/project/qampus/service/security/CustomUserDetailsService.java
+++ backend/backend-qampus-app/src/main/java/com/project/qampus/service/security/CustomUserDetailsService.java
@@ -2,7 +2,6 @@ package com.project.qampus.service.security;
 
 import com.project.qampus.model.User;
 import com.project.qampus.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
